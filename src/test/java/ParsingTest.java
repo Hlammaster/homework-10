9dd54d5ed3e5a7b446480a7a549e048b118316a5
Johnny Firo <hlammaster@mail.ru>
2023-03-28 15:22:57 +0300

    added xlsparsing

--- src/test/java/ParsingTest.java
+++ src/test/java/ParsingTest.java
@@ -1,25 +1,67 @@
 import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ParsingTest {
     private ClassLoader cl = ParsingTest.class.getClassLoader();
+
     @Test
-    void zipTest throws Exception(){
+    void zipCsvParsingTest() throws Exception {
         try (InputStream is = cl.getResourceAsStream("ziptest.zip");
-        ZipInputStream zs = new ZipInputStream(is)) {
+             ZipInputStream zs = new ZipInputStream(is)) {
             ZipEntry entry;
-            while ((entry = zs.getNextEntry())) !=null {
-                if(entry.getName().equals("junit-user-guide-5.9.2.pdf")){
-                    PDF pdf = new PDF(zs);
-                    Assertions.assertEquals("JUnit 5 User Guide");
+            while ((entry = zs.getNextEntry()) != null) {
+                if (entry.getName().equals("test.csv")) {
+                    CSVReader csvReader = new CSVReader(new InputStreamReader(zs));
+                    List<String[]> string = csvReader.readAll();
+                    Assertions.assertArrayEquals(new String[]{"Morrowind", "RPG"}, string.get(1));
+
+
                 }
             }
-         }
+        }
+
+    }
+
+
+    @Test
+    void zipPdfParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("ziptest.zip");
+             ZipInputStream zs = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zs.getNextEntry()) != null) {
+                if (entry.getName().equals("junit-user-guide-5.9.2.pdf")) {
+                    PDF pdf = new PDF(zs);
+                    Assertions.assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein",
+                            pdf.author);
+                }
+            }
+        }
+
+
+    }
+    @Test
+    void zipXlsParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("ziptest.zip");
+             ZipInputStream zs = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zs.getNextEntry()) != null) {
+                if (entry.getName().equals("Grafik_otpuskov_2016_full_holidays (1).xls\"")) {
+                    XLS xls = new XLS(zs);
+                    Assertions.assertTrue(xls.excel.getSheetAt(0).getRow(8).getCell(1).getStringCellValue().startsWith("Сотрудник"));
+
+                }
+            }
+        }
+
 
     }
 
