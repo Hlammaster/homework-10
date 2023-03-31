ea2da567d2b620fe34b2715ca4c0ede894502de6
Johnny Firo <hlammaster@mail.ru>
2023-03-31 14:14:35 +0300

    added blablacommit

--- src/test/java/ParsingTest.java
+++ src/test/java/ParsingTest.java
@@ -2,12 +2,15 @@ import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class ParsingTest {
@@ -15,11 +18,13 @@ public class ParsingTest {
 
     @Test
     void zipCsvParsingTest() throws Exception {
+        ZipFile zipFile = new ZipFile(new File("src/test/resources/ziptest.zip"));
         try (InputStream is = cl.getResourceAsStream("ziptest.zip");
              ZipInputStream zs = new ZipInputStream(is)) {
             ZipEntry entry;
             while ((entry = zs.getNextEntry()) != null) {
-                if (entry.getName().equals("test.csv")) {
+                if (entry.getName().equals("test.csv"))
+                try (InputStream inputStreamCsv = zipFile.getInputStream(entry)){
                     CSVReader csvReader = new CSVReader(new InputStreamReader(zs));
                     List<String[]> string = csvReader.readAll();
                     Assertions.assertArrayEquals(new String[]{"Morrowind", "RPG"}, string.get(1));
@@ -30,19 +35,19 @@ public class ParsingTest {
         }
 
     }
-
-
     @Test
     void zipPdfParsingTest() throws Exception {
+        ZipFile zipFile = new ZipFile(new File("src/test/resources/ziptest.zip"));
         try (InputStream is = cl.getResourceAsStream("ziptest.zip");
              ZipInputStream zs = new ZipInputStream(is)) {
             ZipEntry entry;
             while ((entry = zs.getNextEntry()) != null) {
-                if (entry.getName().equals("junit-user-guide-5.9.2.pdf")) {
-                    PDF pdf = new PDF(zs);
-                    Assertions.assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein",
-                            pdf.author);
-                }
+                if (entry.getName().equals("junit-user-guide-5.9.2.pdf"))
+                    try (InputStream inputStreamPdf = zipFile.getInputStream(entry)){
+                        PDF newPdf = new PDF(inputStreamPdf);
+                        Assertions.assertEquals("JUnit 5 User Guide",newPdf.title);
+
+                    }
             }
         }
 
@@ -50,20 +55,20 @@ public class ParsingTest {
     }
     @Test
     void zipXlsParsingTest() throws Exception {
+        ZipFile zipFile = new ZipFile(new File("src/test/resources/ziptest.zip"));
         try (InputStream is = cl.getResourceAsStream("ziptest.zip");
              ZipInputStream zs = new ZipInputStream(is)) {
             ZipEntry entry;
             while ((entry = zs.getNextEntry()) != null) {
-                if (entry.getName().equals("Grafik_otpuskov_2016_full_holidays (1).xls\"")) {
-                    XLS xls = new XLS(zs);
-                    Assertions.assertTrue(xls.excel.getSheetAt(0).getRow(8).getCell(1).getStringCellValue().startsWith("Сотрудник"));
+                if (entry.getName().equals("123.xls"))
+                    try (InputStream inputStreamXls = zipFile.getInputStream(entry)) {
+                        XLS xls = new XLS(inputStreamXls);
+                        System.out.println();
 
-                }
+                    }
             }
         }
-
 
     }
 
 }
-
---
+++ src/test/java/blaclass.java
@@ -0,0 +1,55 @@
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.xlstest.XLS;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.xlstest.XLS.containsText;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class blaclass {
+
+
+    private ClassLoader cl = blaclass.class.getClassLoader();
+    @Test
+    void pdfTest() throws Exception {
+        InputStream stream = cl.getResourceAsStream("junit-user-guide-5.9.2.pdf");
+        PDF pdf = new PDF(stream);
+        Assertions.assertEquals("JUnit 5 User Guide",pdf.title);
+
+    }
+    @Test
+    void csvParseTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("test.csv");
+             InputStreamReader isr = new InputStreamReader(is)) {
+            CSVReader csvReader = new CSVReader(isr);
+            List<String[]> content = csvReader.readAll();
+            System.out.println();
+            Assertions.assertArrayEquals(new String[] {"Morrowind", "RPG"}, content.get(1));
+        }
+    }
+
+    @Test
+    void xlsTest() throws Exception {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("123.xls");
+        XLS xls = new XLS(stream);
+        System.out.println();
+    }
+
+
+}
+
+
